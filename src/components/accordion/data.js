7478bd3902e40ca9d008fd33cc8bcbda86336d1d
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2024-04-09 01:38:35 +0200

    single selection function and styles added

--- src/components/accordion/data.js
+++ src/components/accordion/data.js
@@ -1,29 +1,29 @@
 const data = [
     {
         id: "1",
-        Language: "Javascript",
-        Details: "JavaScript is a high-level programming language that is one of the core technologies of the World Wide Web. It is used as a client-side programming language by 97.8 percent of all websites.",
+        language: "Javascript",
+        details: "JavaScript is a high-level programming language that is one of the core technologies of the World Wide Web. It is used as a client-side programming language by 97.8 percent of all websites.",
 
     },
     {
         id: "2",
-        Language: "Python",
-        Details: "Python is one of the most popular programming languages today and is easy for beginners to learn because of its readability. It is a free, open-source programming language with extensive support modules and community development, easy integration with web services.",
+        language: "Python",
+        details: "Python is one of the most popular programming languages today and is easy for beginners to learn because of its readability. It is a free, open-source programming language with extensive support modules and community development, easy integration with web services.",
     },
     {
         id: "3",
-        Language: "Java",
-        Details: "Owned by Oracle Corporation, this general-purpose programming language with its object-oriented structure has become a standard for applications that can be used regardless of platform.",
+        language: "Java",
+        details: "Owned by Oracle Corporation, this general-purpose programming language with its object-oriented structure has become a standard for applications that can be used regardless of platform.",
     },
     {
         id : "4",
-        Language: "PHP",
-        Details: "Many web developers find it essential to learn PHP, as this language is used to build more than 80% of websites on the Internet, including notable sites like Facebook and Yahoo.",
+        language: "PHP",
+        details: "Many web developers find it essential to learn PHP, as this language is used to build more than 80% of websites on the Internet, including notable sites like Facebook and Yahoo.",
     },
     {
         id : "5",
-        Language : "C#",
-        Details: "Developed by Microsoft, C# rose to fame in the 2000s for supporting the concepts of object-oriented programming. It is one of the most used programming languages for the .NET framework.",
+        language : "C#",
+        details: "Developed by Microsoft, C# rose to fame in the 2000s for supporting the concepts of object-oriented programming. It is one of the most used programming languages for the .NET framework.",
     }
 ];
 
--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,29 +1,37 @@
 // i have created components folder and inside created accordion component folder.
-// i am doing this to create a usable component to use it in various places. 
-import {useState} from "react";
-import data from "./data";
-export default function Accordion(){
-    const [selected, setSelected] = useState(null);
-    return (
-        <div className="wrapper">
-            <div className="accordion">
-                {
-                    (data && data.length > 0 ? (
-                        data.map((dataitem)=>(
-                            <div className="item">
-                                <div className="title">
-                                    <h3>(dataitem.Language)</h3>
-                                    <span>+</span>
+// i am doing this to create a usable component to use it in various places.
+import { useState } from "react";
+import data from "./data.js";
+import "./styles.css";
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
 
-                            </div>
-                            </div>
-                            
-                        ))
-                    ) : (<div>No data found !</div>)
-                    )
-                }
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+    console.log(selected);
+  }
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.language}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.details}</div>
+              ) : null}
             </div>
-        </div>
-    )
-    
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </div>
+  );
 }
